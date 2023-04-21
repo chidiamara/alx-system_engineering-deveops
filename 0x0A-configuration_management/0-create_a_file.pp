a47593f7cae59c7f4550efe4bf4586e5fe70a30f
Chidi <chidiekejiuba@gmail.com>
2023-04-21 20:07:25 +0100

    fixed with puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # puppet manifest creates a file in /tmp with the following attributes
 file { '/tmp/school':
- mode    => '0774',
- owner   => 'www-data',
- group   => 'www-data',
- content => 'I love Puppet',
+mode    => '0774',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
-
