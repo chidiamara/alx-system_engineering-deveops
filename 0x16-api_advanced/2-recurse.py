235dd67a4fedbf89ffc29ee53352e99d2590e30b
Chidi <chidiamaraekejiuba@gmail.com>
2023-06-09 03:48:02 +0100

    solution update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,16 +4,37 @@
 Queries the Reddit API and returns a list containing
 the titles of all hot articles for a given subreddit.
 """
+import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """Returns a list of titles of all hot articles"""
-    import requests
+    """
+    Recursively retrieves the titles of all hot
+    articles from a given subreddit.
 
+    Args:
+        subreddit (str): The name of the subreddit.
+        hot_list (list, optional): A list to store the titles of hot
+        articles (default is an empty list).
+        after (str, optional): The 'after' parameter to fetch the next set
+        of articles (default is None).
+
+    Returns:
+        list: A list of titles of all hot articles.
+
+    Raises:
+        None
+
+    Example Usage:
+        >>> titles = recurse('python')
+        >>> for title in titles:
+        ...     print(title)
+
+    """
     if not isinstance(subreddit, str) or subreddit is None:
         return None
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     params = {'after': after}
     response = requests.get(url, headers=headers, params=params)
