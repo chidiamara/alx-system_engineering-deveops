7b8d7e02b39aeaf0e9ae881af62aebfa7c01ed5e
Chidi <chidiamaraekejiuba@gmail.com>
2023-06-09 03:40:04 +0100

    solution update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,17 +2,35 @@
 """
 Queries the Reddit API and prints the titles of the first 10 hot posts
 """
-
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts"""
+    """
+    Prints the titles of the first 10 hot posts from a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    Example Usage:
+        >>> top_ten('python')
+        Post 1
+        Post 2
+        ...
+        Post 10
+
+    """
     if not isinstance(subreddit, str) or subreddit is None:
         print("None")
         return
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     response = requests.get(url, headers=headers)
     data = response.json().get('data')
