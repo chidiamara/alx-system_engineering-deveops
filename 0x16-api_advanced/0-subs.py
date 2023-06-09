85c1cbf88a8dd63b751b05d76fe97467d572ebac
Chidi <chidiamaraekejiuba@gmail.com>
2023-06-09 03:22:28 +0100

    solution update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,18 +3,34 @@
 Queries the Reddit API and returns the number of subscribers
 """
 
-import requests
-
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers"""
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if an error occurs.
+
+    Raises:
+        None
+
+    Example Usage:
+        >>> number_of_subscribers('python')
+        8000000
+
+    """
+    import requests
     if not isinstance(subreddit, str) or subreddit is None:
         return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     response = requests.get(url, headers=headers)
+
     try:
         return response.json().get('data').get('subscribers')
-
     except Exception:
         return 0
