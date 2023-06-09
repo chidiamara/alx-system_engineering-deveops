f288ee1b668ad7467e1abbe00e1de7704f837a1b
Chidi <chidiamaraekejiuba@gmail.com>
2023-06-09 04:01:07 +0100

    solution update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,20 +3,44 @@
 Queries the Reddit API, parses the title of all hot
 articles, and prints a sorted count of given keywords
 """
-
 import requests
 
 
 def count_words(subreddit, word_list, after=None, word_dict=None):
+    """
+    Parses the titles of all hot articles in a subreddit
+    and prints a sorted count of specified words.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+        word_list (list): A list of words to count in the article titles.
+        after (str, optional): The 'after' parameter to fetch the next
+        set of articles (default is None).
+        word_dict (dict, optional): A dictionary to store the word counts
+        (default is None).
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    Example Usage:
+        >>> word_list = ['python', 'programming', 'code']
+        >>> count_words('learnprogramming', word_list)
+
+    """
+
     if not isinstance(subreddit, str) or subreddit is None:
         return None
+
     if not isinstance(word_list, list) or word_list is None:
         return None
 
     if word_dict is None:
         word_dict = {word.lower(): 0 for word in word_list}
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     params = {'after': after}
     response = requests.get(url, headers=headers, params=params)
@@ -41,7 +65,7 @@ def count_words(subreddit, word_list, after=None, word_dict=None):
 
             for word, count in sorted_words:
                 if count > 0:
-                    print(f"{word}: {count}")
+                    print("{}: {}".format(word, count))
 
             return
 
